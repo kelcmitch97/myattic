c115de4dce6f9961297133203a41eae0b42c9914
Don McLean <mclean@utoronto.ca>
2022-05-30 18:14:56 -0400

    incorporates seeds, categories for myattic, more to follow

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,3 +1,5 @@
+// Don's edits to incorporate myattic images seeds, categories 0-5
+// description, price, quantity (add some estimates, though actually 1 at present)
 const db = require('./connection');
 const { User, Product, Category } = require('../models');
 
@@ -5,11 +7,12 @@ db.once('open', async () => {
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
-    { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
-    { name: 'Toys' }
+    { name: 'Artwork' },
+    { name: 'Furniture' },
+    { name: 'Glassware' },
+    { name: 'Housewares' },
+    { name: 'Instruments' },
+    { name: 'Tableware' }
   ]);
 
   console.log('categories seeded');
@@ -18,61 +21,61 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Tin of Cookies',
+      name: 'Woven Basket with Handle',
       description:
-        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
-      category: categories[0]._id,
+        'One of our many baskets in all shapes and sizes',
+      image: 'basket-woven.jpg',
+      category: categories[3]._id,
       price: 2.99,
       quantity: 500
     },
     {
-      name: 'Canned Coffee',
+      name: 'BluRay Disc Audio/Video Player',
       description:
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'canned-coffee.jpg',
-      category: categories[0]._id,
+      image: 'bluray-player.jpg',
+      category: categories[4]._id,
       price: 1.99,
       quantity: 500
     },
     {
-      name: 'Toilet Paper',
-      category: categories[1]._id,
+      name: 'Wire Centrepiece',
+      category: categories[0]._id,
       description:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'toilet-paper.jpg',
+      image: 'centrepiece-wire.jpg',
       price: 7.99,
       quantity: 20
     },
     {
-      name: 'Handmade Soap',
-      category: categories[1]._id,
+      name: 'Ceramic Vase',
+      category: categories[5]._id,
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'soap.jpg',
+      image: 'ceramic-vase.jpg',
       price: 3.99,
       quantity: 50
     },
     {
-      name: 'Set of Wooden Spoons',
-      category: categories[1]._id,
+      name: 'China Bowl',
+      category: categories[5]._id,
       description:
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'wooden-spoons.jpg',
+      image: 'china-bowl.jpg',
       price: 14.99,
       quantity: 100
     },
     {
-      name: 'Camera',
-      category: categories[2]._id,
+      name: 'Console Table',
+      category: categories[1]._id,
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
+      image: 'console-table.jpg',
       price: 399.99,
       quantity: 30
     },
     {
-      name: 'Tablet',
+      name: 'Crystal Bowl',
       category: categories[2]._id,
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
@@ -81,49 +84,158 @@ db.once('open', async () => {
       quantity: 30
     },
     {
-      name: 'Tales at Bedtime',
-      category: categories[3]._id,
+      name: 'Waterford Brandy Glass',
+      category: categories[2]._id,
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'bedtime-book.jpg',
+      image: 'brandy-glass.jpg',
       price: 9.99,
       quantity: 100
     },
     {
-      name: 'Spinning Top',
-      category: categories[4]._id,
+      name: 'Waterford Claret Glass',
+      category: categories[2]._id,
       description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
+      image: 'claret-glass.jpg',
       price: 1.99,
       quantity: 1000
     },
     {
-      name: 'Set of Plastic Horses',
-      category: categories[4]._id,
+      name: 'Drum Table',
+      category: categories[1]._id,
       description:
         'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
+      image: 'drum-table.jpg',
       price: 2.99,
       quantity: 1000
     },
     {
-      name: 'Teddy Bear',
+      name: 'Electronic Keyboard',
       category: categories[4]._id,
       description:
         'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
+      image: 'electronic-keyboard.jpg',
       price: 7.99,
       quantity: 100
     },
     {
-      name: 'Alphabet Blocks',
+      name: 'Farmstyle Rocking Chair',
+      category: categories[1]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'farmstyle-rocker.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: 'Flatiron Building Stitchwork',
+      category: categories[0]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'flatiron-building-stitchwork.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: 'Flatware Set',
+      category: categories[5]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'flatware-set.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: 'Korean Secret Box',
+      category: categories[3]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'korean-box.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: 'Mini Tools Set',
+      category: categories[3]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'mini-tools.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: 'Napkin Rings',
+      category: categories[5]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'napkin-rings.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: 'Nursing Rocking Chair',
+      category: categories[1]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'nursing-rocker.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: 'Oil Painting',
+      category: categories[0]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'oil-painting.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: 'Pants Press',
+      category: categories[2]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'pants-press.jpg',
+      price: 9.99,
+      quantity: 600
+    },   
+    {
+      name: 'Piano Chair',
+      category: categories[1]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'piano-chair.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: 'Pill Box',
+      category: categories[3]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'pill-box.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: 'Salt & Pepper Set',
+      category: categories[5]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'salt&pepper.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: 'Woofer Speaker',
       category: categories[4]._id,
       description:
         'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
+      image: 'woofer-speaker.jpg',
       price: 9.99,
       quantity: 600
-    }
+    },
+  
   ]);
 
   console.log('products seeded');
