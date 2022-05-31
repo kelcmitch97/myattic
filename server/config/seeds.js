00bfb4acd485ffc8d2b8272f76f50fa4707ac235
Don McLean <mclean@utoronto.ca>
2022-05-31 12:46:23 -0400

    update seeds descriptions price quantity

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,5 +1,5 @@
-// Don's edits to incorporate myattic images seeds, categories 0-5
-// description, price, quantity (add some estimates, though actually 1 at present)
+// Edits to incorporate myattic images seeds, categories 0-5, now fully loaded...
+// description, price, quantity (fake estimates; images represent a unique item as is)
 const db = require('./connection');
 const { User, Product, Category } = require('../models');
 
@@ -26,85 +26,85 @@ db.once('open', async () => {
         'One of our many baskets in all shapes and sizes',
       image: 'basket-woven.jpg',
       category: categories[3]._id,
-      price: 2.99,
-      quantity: 500
+      price: 9.99,
+      quantity: 100
     },
     {
       name: 'BluRay Disc Audio/Video Player',
       description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
+        'Sony Blu-Ray Disc Audio/Video Player, connects to SurroundSound system--one of many classic stereo system components from myattic.',
       image: 'bluray-player.jpg',
       category: categories[4]._id,
-      price: 1.99,
-      quantity: 500
+      price: 49.99,
+      quantity: 10
     },
     {
       name: 'Wire Centrepiece',
       category: categories[0]._id,
       description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
+        'One of many senior-citizen arts and crafts works from myattic to your table',
       image: 'centrepiece-wire.jpg',
-      price: 7.99,
-      quantity: 20
+      price: 4.99,
+      quantity: 25
     },
     {
       name: 'Ceramic Vase',
       category: categories[5]._id,
       description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
+        'Lovely blue-flower ceramic vase 10-inch height; one of many such items.',
       image: 'ceramic-vase.jpg',
-      price: 3.99,
+      price: 19.99,
       quantity: 50
     },
     {
       name: 'China Bowl',
       category: categories[5]._id,
       description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
+        'White cabbage-leaf design in bone china, 10-inch diameter',
       image: 'china-bowl.jpg',
-      price: 14.99,
-      quantity: 100
+      price: 19.99,
+      quantity: 30
     },
     {
       name: 'Console Table',
       category: categories[1]._id,
       description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
+        'Classic console table from front hall or dining room sideboard, interior shelf; myttic has several similar',
       image: 'console-table.jpg',
-      price: 399.99,
-      quantity: 30
+      price: 199.99,
+      quantity: 10
     },
     {
       name: 'Crystal Bowl',
       category: categories[2]._id,
       description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
+        'Crystal bowl in classic cross-and-olive pattern; myattic has several similar items',
       image: 'tablet.jpg',
-      price: 199.99,
-      quantity: 30
+      price: 39.99,
+      quantity: 20
     },
     {
       name: 'Waterford Brandy Glass',
       category: categories[2]._id,
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
+        'Waterford Crystal: 12-oz brandy glass; beautiful Colleen pattern; myattic has links to most rare Waterford items, including some complete sets!',
       image: 'brandy-glass.jpg',
-      price: 9.99,
-      quantity: 100
+      price: 69.99,
+      quantity: 50
     },
     {
       name: 'Waterford Claret Glass',
       category: categories[2]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
+      description: 'Waterford Crystal: 4.5-oz claret glass; classic Colleen pattern; myattic has links to most rare Waterford items, including some complete sets!',
       image: 'claret-glass.jpg',
-      price: 1.99,
-      quantity: 1000
+      price: 69.99,
+      quantity: 100
     },
     {
       name: 'Drum Table',
       category: categories[1]._id,
       description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
+        'Everyone should have one of these; holds a lamp on top, games and other stuff inside; also your late-aunts will!',
       image: 'drum-table.jpg',
       price: 2.99,
       quantity: 1000
@@ -113,79 +113,79 @@ db.once('open', async () => {
       name: 'Electronic Keyboard',
       category: categories[4]._id,
       description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
+        'Roland electronic digital keyboard, full 88-key weighted-action keyboard, 3-pedals, MIDI interace.',
       image: 'electronic-keyboard.jpg',
-      price: 7.99,
-      quantity: 100
+      price: 399.99,
+      quantity: 10
     },
     {
       name: 'Farmstyle Rocking Chair',
       category: categories[1]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Farmstyle rocking chair from the days when chairs were comfortable; fantastic.',
       image: 'farmstyle-rocker.jpg',
       price: 9.99,
-      quantity: 600
+      quantity: 20
     },
     {
       name: 'Flatiron Building Stitchwork',
       category: categories[0]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Another example of charming seniors arts & crafts from myattic; lots of different items',
       image: 'flatiron-building-stitchwork.jpg',
       price: 9.99,
-      quantity: 600
+      quantity: 200
     },
     {
       name: 'Flatware Set',
       category: categories[5]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Flatware Set 12-place setting, this one with lots of extras: knives, forks, spoons, serving items; my attic has lots of these, but this one is unique',
       image: 'flatware-set.jpg',
-      price: 9.99,
-      quantity: 600
+      price: 199.99,
+      quantity: 1
     },
     {
       name: 'Korean Secret Box',
       category: categories[3]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Beautiful piece of Korean arts & crafts; opens up to reveal push-out secret drawers; great and practical coffee-table conversation piece.',
       image: 'korean-box.jpg',
-      price: 9.99,
-      quantity: 600
+      price: 74.99,
+      quantity: 1
     },
     {
       name: 'Mini Tools Set',
       category: categories[3]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Mini Tools Set: every attic has a few tools to pass on; this grouping one 100s from myattic.',
       image: 'mini-tools.jpg',
-      price: 9.99,
-      quantity: 600
+      price: 29.99,
+      quantity: 400
     },
     {
       name: 'Napkin Rings',
       category: categories[5]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Napkin Rings, set of four, engraved metal; really helps set table!',
       image: 'napkin-rings.jpg',
-      price: 9.99,
-      quantity: 600
+      price: 12.99,
+      quantity: 200
     },
     {
       name: 'Nursing Rocking Chair',
       category: categories[1]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Beautiful low-to-ground rocker; originally designed from nursing mothers; this one unique but we keep looking.',
       image: 'nursing-rocker.jpg',
-      price: 9.99,
-      quantity: 600
+      price: 98.99,
+      quantity: 1
     },
     {
       name: 'Oil Painting',
       category: categories[0]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Oil Painting: this one by John E Rutherford, a brooding landscape of "Tralee" in Ireland; my attic has 100s of artworks such as this; a separate catalog link is under development.',
       image: 'oil-painting.jpg',
       price: 9.99,
       quantity: 600
@@ -194,46 +194,46 @@ db.once('open', async () => {
       name: 'Pants Press',
       category: categories[2]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Corby Pants Press: yes, this is the original you sometimes find in hotel rooms and wonder, heh? Well, here it is from myattic.',
       image: 'pants-press.jpg',
-      price: 9.99,
-      quantity: 600
+      price: 49.00,
+      quantity: 15
     },   
     {
       name: 'Piano Chair',
       category: categories[1]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Piano Chair: very solid, height-adjustable, as used by professional players; from my attic at 1/4 the price.',
       image: 'piano-chair.jpg',
-      price: 9.99,
-      quantity: 600
+      price: 99.99,
+      quantity: 50
     },
     {
       name: 'Pill Box',
       category: categories[3]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Pill box: myattic loves finding this sort of simple, practical item; charming.',
       image: 'pill-box.jpg',
       price: 9.99,
-      quantity: 600
+      quantity: 40
     },
     {
       name: 'Salt & Pepper Set',
       category: categories[5]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Salt & Pepper Set; we love this one, and we make many other such classic S&P designs from the previous millenium available to you!',
       image: 'salt&pepper.jpg',
-      price: 9.99,
-      quantity: 600
+      price: 29.99,
+      quantity: 60
     },
     {
       name: 'Woofer Speaker',
       category: categories[4]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+        'Woofer Speaker: from Sony surround-sound speaker set; myattic has access to parts and full systems.',
       image: 'woofer-speaker.jpg',
-      price: 9.99,
-      quantity: 600
+      price: 39.99,
+      quantity: 40
     },
   
   ]);
