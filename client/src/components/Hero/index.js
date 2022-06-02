8248711602467a6d38c141a19cb542c6b744a052
EvanAB14 <evan.a.boileau14@gmail.com>
2022-06-01 20:21:09 -0400

    added hero

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -4,7 +4,22 @@ import "./style.css"
 const Hero = () => {
     return (
         <div className="hero-banner">
-            <div className="left-side"></div>
+            <div className="left-side">
+
+                <h1>Test Header</h1>
+
+                <p>Slogan goes here</p>
+
+                <button>Our Products</button>
+
+            </div>
+
+            <div className="right-side">
+
+
+
+            </div>
+
         </div>
     );
 }
