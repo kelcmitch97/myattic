c6ab5d5e99251f9fd35225404c8cd4f7382848b0
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-06-01 22:35:06 -0230

    Add hero banner

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import "./style.css"
+import { Link } from 'react-router-dom';
+import ProductList from '../ProductList';
+import HeroImg from '../../assets/hero2.PNG';
 
 const Hero = () => {
     return (
+        
         <div className="hero-banner">
             <div className="left-side">
 
-                <h1>Test Header</h1>
+                <h1>Upload Your</h1>
+                <h2>Downsize</h2>
 
-                <p>Slogan goes here</p>
+                <p>Reduce waste and repurpose classic charming hard-to-find stuff!</p>
 
-                <button>Our Products</button>
+                <Link to="/productlist"><button type="submit">Our Products</button></Link>
+
 
             </div>
 
             <div className="right-side">
-
-
-
+                <img src={HeroImg} alt='hero image'></img>
             </div>
 
         </div>
