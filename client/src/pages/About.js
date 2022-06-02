3f18ad0604b39765da61180d08e04a1b0ffcd070
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-06-02 12:24:51 -0230

    Add about content

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,18 +3,23 @@ import React from 'react'
 const About = () => {
     return (
         <div className='about-page-container'>
-            <div className='about-content'>
+            <div className='about-title'>
+            <h1>About My Attic...</h1>
+            </div>
 
-                <h1>About My Attic...</h1>
+            <div className='about-content'>
 
                 <div className='about-left-side'>
                     <p>
-
+                    MyAttic is an ecommerce application that provides a widely-needed public service to connect communities to reduce waste and repurpose their ‘stuff’. Think of it as an online ‘garage sale’. People—from millennials to seniors—moving to smaller spaces look for help through the stressful process of downsizing. And those who find themselves inheriting the stuff of family and friends need a service that can help them sell no-longer-needed items to others who may be looking for classic, charming, and unique things. Welcome to MyAttic!
+                    </p>
+                    <p>
+                    MyAttic buyers and sellers login/register on the site and can view the Products and Categories for sale. Buyers… can select items, place them in their shopping cart and pay by credit card through our secure online system. Sellers… can post items to MyAttic via their contact form by name, description, image (upload 640px wide image in standard format, we tend to use .jpg), and suggested price. MyAttic can assist you if needed and will let you know when you have a buyer.
                     </p>
                 </div>
                 <div className='about-right-side'>
                     <p>
-
+                    Shipping… costs are generally passed on to the buyer but are kept minimal through our partnerships with standard overland couriers and moving companies (for larger items). We can help sellers with good local shipping contacts, packaging materials and tips. Once the transaction is completed a shipping label will be provided to the seller to send the item directly to the buyer.
                     </p>
                 </div>
 
